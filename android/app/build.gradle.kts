c95a6194654cc468a01bd75fd30148edd8539cec
Kavish Devar <mail@kavishdevar.me>
2025-05-19 17:39:55 +0530

    android: bump version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "me.kavishdevar.librepods"
         minSdk = 28
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.1.0-rc.3"
+        versionCode = 7
+        versionName = "0.1.0-rc.4"
     }
 
     buildTypes {
